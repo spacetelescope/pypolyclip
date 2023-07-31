f448d0b8ce9fccdadea29ff969e511eb1e3c74e5
Larry Bradley <larry.bradley@gmail.com>
2023-07-31 17:10:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
-from glob import glob
-import numpy as np
 import os
-from setuptools import setup,Extension
+from glob import glob
+
+import numpy as np
+from setuptools import Extension, setup
 
 
-def main():
-    path=('pypolyclip',)
+path = ('pypolyclip',)
+ext = Extension('.'.join(path) + '.polyclip',
+                glob(os.path.join(*path, 'src', '*.c')),
+                include_dirs=[os.path.join(*path, 'include'),
+                              np.get_include()])
 
-    
-    ext=Extension('.'.join(path)+'.polyclip',
-                  glob(os.path.join(*path,'src','*.c')),
-                  include_dirs=[os.path.join(*path,'include'),
-                                np.get_include()])
-    
-    setup(ext_modules=[ext])
-
-
-if __name__=='__main__':
-    main()
+setup(ext_modules=[ext])
