c78e22b7693205d31393c48417215f0a040d36d3
Larry Bradley <larry.bradley@gmail.com>
2023-09-20 11:49:19 -0400

    Minor simplification of setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from glob import glob
 import numpy as np
 from setuptools import Extension, setup
 
-path = ('pypolyclip',)
-ext = Extension('.'.join(path) + '.polyclip',
-                glob(os.path.join(*path, 'src', '*.c')),
-                include_dirs=[os.path.join(*path, 'include'),
+package = 'pypolyclip'
+ext = Extension(f'{package}.polyclip',
+                glob(os.path.join(package, 'src', '*.c')),
+                include_dirs=[os.path.join(package, 'include'),
                               np.get_include()])
 
 setup(ext_modules=[ext])
