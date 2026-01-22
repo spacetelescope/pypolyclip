6598a5ba65adb1662e1c5e89429941770a16ffd9
Larry Bradley <larry.bradley@gmail.com>
2026-01-22 14:43:55 -0500

    Add license lines

--- pypolyclip/__init__.py
+++ pypolyclip/__init__.py
@@ -1,3 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 Pypolyclip is a Python library for clipping polygons against a pixel
 grid.
--- pypolyclip/pypolyclip.py
+++ pypolyclip/pypolyclip.py
@@ -1,3 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 Module that provides a Python interface to polyclip.c.
 
