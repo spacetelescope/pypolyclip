45ddf5bcc1bd5cc06551577cc810516b3f229149
Larry Bradley <larry.bradley@gmail.com>
2026-09-08 19:47:05 -0400

    Enable limited API builds

--- setup.py
+++ setup.py
@@ -9,9 +9,15 @@ import numpy as np
 from setuptools import Extension, setup
 
 package = 'pypolyclip'
+
+# Build against the Python limited API (stable ABI) for Python 3.11+
+# so that a single abi3 wheel works on all supported Python versions.
+# The wheel tag is set by "py-limited-api" in pyproject.toml.
 ext = Extension(f'{package}.polyclip',
                 glob(os.path.join(package, 'src', '*.c')),
                 include_dirs=[os.path.join(package, 'include'),
-                              np.get_include()])
+                              np.get_include()],
+                define_macros=[('Py_LIMITED_API', '0x030B0000')],
+                py_limited_api=True)
 
 setup(ext_modules=[ext])
